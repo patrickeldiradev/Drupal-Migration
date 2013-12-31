1bfcc58a31ba0e77b32f9bccdec6e58cca8b71cb
Onur Rauf Bingol <bingol@metu.edu.tr>
2014-01-01 00:00:00 +0500

    Translation fix Drush command is added.

--- .drush/drushrc.php
+++ .drush/drushrc.php
@@ -2,3 +2,4 @@
 $options['shell-aliases']['convert-fs'] = 'metu-convert-featured-slider';
 $options['shell-aliases']['load-node'] = 'metu-load-node';
 $options['shell-aliases']['convert-vc'] = 'metu-convert-video-content';
+$options['shell-aliases']['fix-tr'] = 'metu-fix-translations';
