ab53148b1612052086d58929f2e959df887071e7
Onur Rauf Bingol <bingol@metu.edu.tr>
2014-01-01 00:00:00 +0500

    Added option to remove defined taxonomy term

--- .drush/drushrc.php
+++ .drush/drushrc.php
@@ -3,3 +3,4 @@ $options['shell-aliases']['convert-fs'] = 'orb-convert-featured-slider';
 $options['shell-aliases']['load-node'] = 'orb-load-node';
 $options['shell-aliases']['convert-vc'] = 'orb-convert-video-content';
 $options['shell-aliases']['fix-tr'] = 'orb-fix-translations';
+$options['shell-aliases']['del-tax'] = 'orb-remove-taxonomy-term';
